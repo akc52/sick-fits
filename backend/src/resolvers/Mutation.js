754c9d3c6f96043b57e3819fcd49d6940684dd73
akc52 <a_lys_sa@hotmail.com>
2018-10-18 08:58:42 -0400

    Items creation and prisma yoga flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,4 +1,16 @@
 const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    //TO DO check logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
+  /*
   createDog(parent, args, ctx, info) {
     console.log(args);
     global.dogs = global.dogs || [];
@@ -6,6 +18,7 @@ const Mutations = {
     global.dogs.push(newDog);
     return newDog;
   }
+  */
 };
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,8 +1,19 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
+  items: forwardTo('dib')
+  /*
+  async items(parent, args, ctx, info) {
+    const items = await ctx.db.query.items();
+    return items;
+  }
+  */
+  /*
   dogs(parent, args, ctx, info) {
     global.dogs = global.dogs || [];
     return global.dogs;
   }
+  */
 };
 
 module.exports = Query;
